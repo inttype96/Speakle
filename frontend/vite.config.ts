4a3fceae98592e27e3398d96367dfa1cefd6b7a2
2perday <ds4rxh@gmail.com>
2025-09-10 15:42:52 +0900

    feat: login 추가

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -16,7 +16,7 @@ export default defineConfig({
     proxy: {
       // '/api'로 시작하는 요청은 모두 백엔드 서버로 전달합니다.
       '/api': {
-        target: 'http://localhost:8080',
+        target: 'http://backend:8080',
         changeOrigin: true, // 다른 origin으로 요청을 보낼 때 필요합니다.
       },
     },
