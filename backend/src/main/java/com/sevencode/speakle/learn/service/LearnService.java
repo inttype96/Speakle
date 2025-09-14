0760c1bea4177b1dc794a98e020705f5dac3d5be
김아윤 <kimay9623@naver.com>
2025-09-14 23:33:18 +0900

    refactor: speaking 게임 문제 조회 기능 코드 위치 정리 - api 동작 변경 없음
    # S13P21C104-119

--- backend/src/main/java/com/sevencode/speakle/learn/service/LearnService.java
+++
@@ -1,7 +0,0 @@
-package com.sevencode.speakle.learn.service;
-
-import com.sevencode.speakle.learn.dto.response.SpeakingQuestionResponse;
-
-public interface LearnService {
-    SpeakingQuestionResponse getSpeakingQuestion(Long learnedSongId, Integer questionNumber, Long userId);
-}
