84223d904640fcde005b20539a0d5ef6293c7d29
2perday <ds4rxh@gmail.com>
2025-09-16 08:40:11 +0900

    fe vite.config 수정

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -17,7 +17,7 @@ export default defineConfig({
     proxy: {
       // '/api'로 시작하는 요청은 모두 백엔드 서버로 전달합니다.
       '/api': {
-        target: 'https://j13c104.p.ssafy.io/api',
+        target: 'https://j13c104.p.ssafy.io',
         changeOrigin: true, // 다른 origin으로 요청을 보낼 때 필요합니다.
         secure: true,
       },
