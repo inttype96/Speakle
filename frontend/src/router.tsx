934b4224574433a9a639d4b6730a61bbec37f093
2perday <ds4rxh@gmail.com>
2025-09-15 10:38:20 +0900

    feat: fe 로그인 api 생성

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 
 import IndexPage from "@/pages/common/indexPage";
 import LoginPage from "@/pages/auth/loginPage";
+import SignupPage from "@/pages/auth/signupPage";
 import TestPage from "@/pages/test/testPage";
 
 const router = createBrowserRouter([
@@ -12,6 +13,10 @@ const router = createBrowserRouter([
     {
         path: "/login",
         element: <LoginPage />,
+    },
+    {
+        path: "/signup",
+        element: <SignupPage />,
     },
     {
         path: "/test",
