8b2ad7fc2aad6e96b8bce40f871a53666f8bb075
2perday <ds4rxh@gmail.com>
2025-09-16 11:00:28 +0900

    fix: spotify oauth 수정

--- frontend/src/pages/common/notFoundPage.tsx
+++ frontend/src/pages/common/notFoundPage.tsx
@@ -4,7 +4,7 @@ export default function NotFoundPage() {
   return (
     <main className="grid min-h-full place-items-center bg-background px-6 py-24 sm:py-32 lg:px-8">
       <div className="text-center">
-        <p className="text-base font-semibold text-primary">404</p>
+        <h2 className="text-base font-semibold text-primary">404</h2>
         <h1 className="mt-4 text-5xl font-semibold tracking-tight text-balance text-foreground sm:text-7xl">
           Page not found
         </h1>
