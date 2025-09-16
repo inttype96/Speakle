308255f50929831a5c5a454e5c3c47a611500856
김아윤 <kimay9623@naver.com>
2025-09-16 15:29:06 +0900

    feat: Point 조회 기능 구현 - 사용자 포인트 잔액 조회 API 추가 - UserNotFoundException 예외 처리 추가
    # S13P21C104-194

--- backend/src/main/java/com/sevencode/speakle/reward/controller/RewardController.java
+++ backend/src/main/java/com/sevencode/speakle/reward/controller/RewardController.java
@@ -3,16 +3,14 @@ package com.sevencode.speakle.reward.controller;
 import com.sevencode.speakle.config.security.UserPrincipal;
 import com.sevencode.speakle.learn.dto.response.ApiResponse;
 import com.sevencode.speakle.reward.dto.request.RewardUpdateRequest;
+import com.sevencode.speakle.reward.dto.response.RewardProfileResponse;
 import com.sevencode.speakle.reward.dto.response.RewardUpdateResponse;
 import com.sevencode.speakle.reward.service.RewardService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -30,5 +28,15 @@ public class RewardController {
         RewardUpdateResponse response = rewardService.updateReward(request, userId);
 
         return ResponseEntity.ok(ApiResponse.success(200, "포인트를 업데이트 했습니다.", response));
+    }
+
+    /**
+     * 포인트 조회
+     */
+    @GetMapping("/profile")
+    public ResponseEntity<ApiResponse<RewardProfileResponse>> getPointProfile(
+            @RequestParam Long userId) {
+        RewardProfileResponse response = rewardService.getPointProfile(userId);
+        return ResponseEntity.ok(ApiResponse.success(200, "포인트 프로필 조회에 성공했습니다.", response));
     }
 }
