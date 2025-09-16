cadde8b4b1f83a08225b9e74c5af39b487e65cee
2perday <ds4rxh@gmail.com>
2025-09-16 11:43:39 +0900

    feat: footer 생성

--- frontend/src/pages/common/footer.tsx
+++ frontend/src/pages/common/footer.tsx
@@ -7,39 +7,39 @@ export default function Footer() {
                 </h2>
                 <div className="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5">
                     <img
-                        alt="Transistor"
-                        src="https://tailwindcss.com/plus-assets/img/logos/158x48/transistor-logo-gray-900.svg"
+                        alt="React"
+                        src="https://simpleicons.org/icons/react.svg"
                         width={158}
                         height={48}
                         className="col-span-2 max-h-12 w-full object-contain lg:col-span-1 dark:invert"
                     />
                     <img
-                        alt="Reform"
-                        src="https://tailwindcss.com/plus-assets/img/logos/158x48/reform-logo-gray-900.svg"
+                        alt="Spring Boot"
+                        src="https://simpleicons.org/icons/springboot.svg"
                         width={158}
                         height={48}
                         className="col-span-2 max-h-12 w-full object-contain lg:col-span-1 dark:invert"
                     />
                     <img
-                        alt="Tuple"
-                        src="https://tailwindcss.com/plus-assets/img/logos/158x48/tuple-logo-gray-900.svg"
+                        alt="Nginx Proxy Manager"
+                        src="https://simpleicons.org/icons/nginxproxymanager.svg"
                         width={158}
                         height={48}
                         className="col-span-2 max-h-12 w-full object-contain lg:col-span-1 dark:invert"
-                    />
-                    <img
-                        alt="SavvyCal"
-                        src="https://tailwindcss.com/plus-assets/img/logos/158x48/savvycal-logo-gray-900.svg"
-                        width={158}
-                        height={48}
-                        className="col-span-2 max-h-12 w-full object-contain sm:col-start-2 lg:col-span-1 dark:invert"
                     />
                     <img
                         alt="Statamic"
-                        src="https://tailwindcss.com/plus-assets/img/logos/158x48/statamic-logo-gray-900.svg"
+                        src="https://simpleicons.org/icons/docker.svg"
                         width={158}
                         height={48}
                         className="col-span-2 col-start-2 max-h-12 w-full object-contain sm:col-start-auto lg:col-span-1 dark:invert"
+                    />
+                    <img
+                        alt="SavvyCal"
+                        src="https://www.svgrepo.com/show/448299/aws.svg"
+                        width={158}
+                        height={48}
+                        className="col-span-2 max-h-12 w-full object-contain sm:col-start-2 lg:col-span-1 dark:invert"
                     />
                 </div>
             </div>
