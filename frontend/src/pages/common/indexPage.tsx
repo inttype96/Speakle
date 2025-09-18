f2dd4a1bed5441af1155fae24dc2bd885a951d8b
2perday <ds4rxh@gmail.com>
2025-09-18 11:06:36 +0900

    fix: mypage 수정

--- frontend/src/pages/common/indexPage.tsx
+++ frontend/src/pages/common/indexPage.tsx
@@ -7,53 +7,33 @@ import Plasma from '@/lib/plasma';
 export default function IndexPage() {
     return (
         <div className="bg-background text-foreground">
-            <div style={{ width: '100%', height: '600px', position: 'relative' }}>
+            <SplashCursor />
+            <Navbar />
+            <div style={{ width: '100%', position: 'relative' }}>
                 <Plasma
-                    color="#ff6b35"
+                    color="#cd35ffff"
                     speed={0.6}
                     direction="forward"
                     scale={1.1}
                     opacity={0.8}
                     mouseInteractive={true}
                 />
-            </div>
-            <SplashCursor />
-            <Navbar />
-
-            {/* Hero Section */}
-            <div className="relative isolate px-6 pt-14 lg:px-8">
-                <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
-                    {/* <div className="hidden sm:mb-8 sm:flex sm:justify-center">
-                        <div className="relative rounded-full px-3 py-1 text-sm text-muted-foreground ring-1 ring-border hover:ring-foreground/40">
-                            Announcing our next round of funding.{" "}
-                            <a href="#" className="font-semibold text-primary">
-                                <span aria-hidden="true" className="absolute inset-0" />
-                                Read more <span aria-hidden="true">&rarr;</span>
-                            </a>
+                {/* Hero Section */}
+                <div className="relative isolate px-6 pt-14 lg:px-8">
+                    <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+                        <div className="text-center">
+                            <h1 className="text-5xl font-bold subpixel-antialiased tracking-tight text-balance sm:text-7xl">
+                                Speakle
+                            </h1>
+                            <p className="mt-8 text-lg font-medium text-muted-foreground sm:text-xl">
+                                음악처럼 스며드는 영어학습. 듣고, 따라하고, 오래 남는 영어
+                            </p>
                         </div>
-                    </div> */}
-                    <div className="text-center">
-                        <h1 className="text-5xl font-bold subpixel-antialiased tracking-tight text-balance sm:text-7xl">
-                            Speakle
-                        </h1>
-                        <p className="mt-8 text-lg font-medium text-muted-foreground sm:text-xl">
-                            음악처럼 스며드는 영어학습. 듣고, 따라하고, 오래 남는 영어
-                        </p>
-                        {/* <div className="mt-10 flex items-center justify-center gap-x-6">
-                            <Button asChild size="lg" className="">
-                                <Link to="/login" className="flex items-center">
-                                    <LockClosedIcon className="h-6 w-6 mr-2" /> 로그인
-                                </Link>
-                            </Button>
-                            <Button variant="outline" size="lg" className="">
-                                <StarIcon className="h-6 w-6 mr-2" /> 더 알아보기
-                            </Button>
-                        </div> */}
                     </div>
+                    <Footer />
                 </div>
             </div>
 
-            <Footer />
         </div>
     )
 }
