013d9234a51fe39adba76987576967ba9c3509a2
2perday <ds4rxh@gmail.com>
2025-09-18 14:38:13 +0900

    fix: docker compose 수정

--- data-service/app/config/settings.py
+++ data-service/app/config/settings.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-WEAVIATE_HOST = os.getenv("WEAVIATE_HOST", "localhost")
+WEAVIATE_HOST = os.getenv("WEAVIATE_HOST", "weaviate")
 WEAVIATE_PORT = int(os.getenv("WEAVIATE_PORT", "8082"))
 WEAVIATE_GRPC_PORT = int(os.getenv("WEAVIATE_GRPC_PORT", "50051"))
 
