5f9cfb5b5630f4720ba66d873ba4f0d5ef9fa318
임덕규 <imdg3530@gmail.com>
2025-09-18 14:29:01 +0900

    fix: 충돌 해결

--- frontend/src/components/ui/toggle-group.tsx
+++ frontend/src/components/ui/toggle-group.tsx
@@ -28,10 +28,6 @@ function ToggleGroup({
       data-variant={variant}
       data-size={size}
       className={cn(
-<<<<<<< HEAD
-        "group/toggle-group flex w-fit items-center rounded-md data-[variant=outline]:shadow-xs",
-=======
->>>>>>> feature/FE-song
         className
       )}
       {...props}
