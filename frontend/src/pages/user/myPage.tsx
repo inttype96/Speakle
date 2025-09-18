79823edf5e0d66d0a58bda90395639f5c4807017
2perday <ds4rxh@gmail.com>
2025-09-18 13:22:28 +0900

    fix: mypage 수정

--- frontend/src/pages/user/myPage.tsx
+++ frontend/src/pages/user/myPage.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react'
 import { useNavigate, useLocation } from 'react-router-dom'
 import { useAuthStore, isAuthenticated } from '@/store/auth'
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+import { Card, CardContent, } from '@/components/ui/card'
 import { Button } from '@/components/ui/button'
 import { Badge } from '@/components/ui/badge'
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
