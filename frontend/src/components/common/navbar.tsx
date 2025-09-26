3d99dae7c6a1c0e39d1826b90c8e508671d9560e
2perday <ds4rxh@gmail.com>
2025-09-26 14:07:32 +0900

    fix: navbar pb, pt 조정

--- frontend/src/components/common/navbar.tsx
+++ frontend/src/components/common/navbar.tsx
@@ -16,7 +16,7 @@ export default function Navbar() {
         <>
             <header className="absolute inset-x-0 top-0 z-50 bg-black">
                 <nav aria-label="Global">
-                    <div className="flex items-center justify-between p-4 lg:p-6 lg:pb-4">
+                    <div className="flex items-center justify-between p-4">
                         {/* 로고 */}
                         <div className="flex">
                             <Link to="/" className="-m-1.5 p-1.5">
